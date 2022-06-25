589d4b9501ad05e612d2f8b4a7e07d45069e9e67
SimonEvs <simonevseev07@mail.ru>
2022-06-25 15:50:07 +0300

    add 3rd task

---
+++ 333/Program.cs
@@ -0,0 +1,23 @@
+﻿int a =0;
+Console.Clear();
+Console.WriteLine("Найдем наибольшее число!");
+Console.WriteLine("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Найдем наибольшее число!");
+Console.WriteLine("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Найдем наибольшее число!");
+Console.WriteLine("Введите третье число: ");
+int third = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write("Ура, вы нашли его, это число!!! ");
+if (first> second)
+a = first;
+else
+a= second;
+
+if (a<third)
+a = third;
+Console.WriteLine("И это число: " + a);
