dd1b5f58b59833b13bf68d21911fac12c33692c9
SimonEvs <simonevseev07@mail.ru>
2022-06-25 16:17:32 +0300

    add 4 task

---
+++ 444/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+double b,d;
+int c;
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+b=a;//4.0
+c=a/2;//2
+d=b/2;//2.0
+Console.Clear();
+if (c==d)
+Console.WriteLine(a + " четное");
+else
+Console.WriteLine(a + " нечетное");
