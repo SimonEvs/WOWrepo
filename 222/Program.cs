1f1f1d3928afa1584e4ab3158fd01f595ef4f028
SimonEvs <simonevseev07@mail.ru>
2022-06-25 15:40:08 +0300

    do the second task

--- 222/Program.cs
+++ 222/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hell31444o, World!");
+﻿Console.Clear();
+Console.WriteLine("Найдем наибольшее число!");
+Console.WriteLine("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Найдем наибольшее число!");
+Console.WriteLine("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write("Ура, вы нашли его, это число: ");
+if (first> second)
+Console.Write(first);
+else
+Console.Write(second);
