f58de4b9d4ac9fd50d2f233d7e576c4be1384809
SimonEvs <simonevseev07@mail.ru>
2022-06-25 16:23:41 +0300

    add 4 task)

--- 444/Program.cs
+++ 444/Program.cs
@@ -3,6 +3,10 @@ double b,d;
 int c;
 Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
+if (a==0)
+Console.WriteLine("Ноль - четное число, гугл говорит так)");
+else
+{
 b=a;//4.0
 c=a/2;//2
 d=b/2;//2.0
@@ -10,4 +14,4 @@ Console.Clear();
 if (c==d)
 Console.WriteLine(a + " четное");
 else
-Console.WriteLine(a + " нечетное");
+Console.WriteLine(a + " нечетное");}
